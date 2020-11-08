dcf12f32778d05487d426a71708f35ed93b2fb82
gradzio <gregradzio@gmail.com>
2020-11-08 19:56:52 +0100

    fixed tests

--- src/app/application/users.state.spec.ts
+++ src/app/application/users.state.spec.ts
@@ -1,15 +1,22 @@
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { UserAggregate } from '../domain/user.aggregate';
+import { UsersService } from '../infrastructure/users.service';
 import { STORE, Store, SubjectStore } from './store';
 import { FetchUsersAction, UsersState } from './users.state';
 
-fdescribe('UserState', () => {
+describe('UserState', () => {
   let store: Store;
   let state: UsersState;
+  let service: UsersService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       providers: [
+        { provide: UsersService, useValue: {
+            getAll: () => of([])
+          }
+        },
         { provide: STORE, useClass: SubjectStore },
         UsersState
       ]
@@ -20,10 +27,12 @@ fdescribe('UserState', () => {
   beforeEach(() => {
     store = TestBed.inject(STORE);
     state = TestBed.inject(UsersState);
+    service = TestBed.inject(UsersService);
   });
 
   it('should fetch users', fakeAsync(() => {
     const userGreg = new UserAggregate('Greg');
+    spyOn(service, 'getAll').and.returnValue(of([userGreg]));
     store.dispatch(new FetchUsersAction());
 
     tick();
